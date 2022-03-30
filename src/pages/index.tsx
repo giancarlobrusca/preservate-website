c1e5a97dbc763def3390abb81403e8d994a9f761
giancarlol <giancarlobrusca@gmail.com>
2022-03-30 18:53:14 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { AppHead, Hero, Donations, About, Navbar, Footer } from '../components';
 export default function Home() {
   return (
     <div className="flex flex-col items-center w-full">
+      a
       <AppHead />
       <Hero />
       <About />
